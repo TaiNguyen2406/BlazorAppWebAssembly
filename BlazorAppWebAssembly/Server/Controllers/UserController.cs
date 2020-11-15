c6137e940237609409919911ab133c4032ff15b6
Tai Nguyen <underwaterhp93@gmail.com>
2020-11-15 22:31:35 +0700

    add getting user data from mock api

---
+++ BlazorAppWebAssembly/Server/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using BlazorAppWebAssembly.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorAppWebAssembly.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        HttpClient _client;
+        public UserController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        [HttpGet]
+        public async  Task<IEnumerable<User>> List()
+        {
+            IEnumerable<User> users = null;
+            HttpResponseMessage response = await _client.GetAsync(
+                  "/users");
+            if (response.IsSuccessStatusCode)
+            {
+                users = await JsonSerializer.DeserializeAsync<IEnumerable<User>>(await response.Content.ReadAsStreamAsync());
+
+            }
+            return users;
+        }
+    }
+}
---
+++ BlazorAppWebAssembly/Shared/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BlazorAppWebAssembly.Shared
+{
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+    }
+}
